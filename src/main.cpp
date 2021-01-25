8b76cd63f7ed2cde74476c8ed357adb4d6054273
Colby Toland <colby@boulderlabs.com>
2021-01-24 18:07:45 -0700

    Progress on using the depthai core lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <algorithm>
 #include <fstream>
+#include <chrono>
 
 #include <opencv2/core/core.hpp>
 
@@ -9,11 +10,11 @@
 
 
 // TODO List:
-//  1. ORB Vocab and Settings file
-//  2. Camera type detection and command line argument overrides
-//  3. Read image code
-//  4. Read IMU code
-//  5. Possible data massaging (e.g. timestamps, depth map size vs RGB image size, etc.)
+//  * ORB Vocab and Settings file
+//  * Command line camera type override
+//  * Read image code
+//  * Read IMU code
+//  * Possible data massaging (e.g. timestamps, depth map size vs RGB image size, etc.)
 
 
 using namespace std;
@@ -21,21 +22,28 @@ using namespace std;
 int main(int argc, char** argv)
 {
     // Setup OAK device
-    Device d("", true);
+    Device d();
     
-    // TODO - Detect OAK-D or OAK-1 or overrid with user settings
-    ORB_SLAM3::System::eSensor camera_type = ORB_SLAM3::System::RGBD;
+    // Detect OAK-D or OAK-1 or override with user settings
+    ORB_SLAM3::System::eSensor camera_type = ORB_SLAM3::System::MONOCULAR;
+    if (d.is_left_connected() && d.is_right_connected())
+        if (!d.is_rgb_connected())
+            camera_type = ORB_SLAM3::System::STEREO;
+        else
+            camera_type = ORB_SLAM3::System::RGBD;
 
     // Create SLAM system. It initializes all system threads and gets ready to process frames.
     ORB_SLAM3::System SLAM(/* ORB Vocab file */, /* Settings file */, camera_type, true);
 
-    // Main loop
     cv::Mat imRGB, imD, imL, imR;
-    vector<ORB_SLAM3::IMU::Point>& vImuMeas
+    //vector<ORB_SLAM3::IMU::Point>& vImuMeas
+    shared_ptr<CNNHostPipeline> pipeline = d.create_pipeline(/* config */)
+
     char key_pressed = 'a'
     while (key_pressed != 'q')
     {
         // Pull data from the camera(s)
+        tuple<list<shared_ptr<NNetPacket> >,list<shared_ptr<HostDataPacket> > > packet = pipeline->getAvailableNNetAndDataPackets(true)
         if (camera_type == ORB_SLAM3::System::MONOCULAR || camera_type == ORB_SLAM3::System::IMU_MONOCULAR || camera_type == ORB_SLAM3::System::RGBD)
         {
             imRGB = // RGB image
@@ -49,12 +57,12 @@ int main(int argc, char** argv)
         {
             imD = // depth map
         }
-        else if(camera_type == ORB_SLAM3::System::IMU_MONOCULAR || camera_type == ORB_SLAM3::System::IMU_STEREO)
-        {
-            vImuMeas = // imu measurement
-        }
+        //else if(camera_type == ORB_SLAM3::System::IMU_MONOCULAR || camera_type == ORB_SLAM3::System::IMU_STEREO)
+        //{
+        //    vImuMeas = // imu measurement
+        //}
 
-        double tframe = // Timestamp of the image(s)
+        double tframe = chrono::system_clock::now();
 
         // Pass the image to the SLAM system
         if (camera_type == ORB_SLAM3::System::MONOCULAR)
@@ -63,10 +71,10 @@ int main(int argc, char** argv)
             SLAM.TrackStereo(imL, imR, tframe);
         else if(camera_type == ORB_SLAM3::System::RGBD)
             SLAM.TrackRGBD(imRGB, imD, tframe);
-        else if(camera_type == ORB_SLAM3::System::IMU_MONOCULAR)
-            SLAM.TrackMonocular(imRGB, tframe, vImuMeas);
-        else if(camera_type == ORB_SLAM3::System::IMU_STEREO)
-            SLAM.TrackStereo(imL, imR, tframe, vImuMeas);
+        //else if(camera_type == ORB_SLAM3::System::IMU_MONOCULAR)
+        //    SLAM.TrackMonocular(imRGB, tframe, vImuMeas);
+        //else if(camera_type == ORB_SLAM3::System::IMU_STEREO)
+        //    SLAM.TrackStereo(imL, imR, tframe, vImuMeas);
         
         key_pressed = getchar();
     }
